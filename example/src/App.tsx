1a2f31bdf99170e8e9d4d705865b3db521a2e760
hochan222 <hochan049@gmail.com>
2021-07-17 16:45:30 +0900

    feat: boiler plate init settings

---
+++ example/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Boilerplate } from 'typescript-react-test';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Boilerplate />
+    </div>
+  );
+}
+
+export default App;
